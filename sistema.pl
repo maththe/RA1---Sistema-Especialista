464f1d3df3f5afb5b13f3b3fa6baf497cf19c82c
Matheus Bernardi <matheusantoniobernardi@gmail.com>
2025-09-28 17:44:37 -0300

    commit interface usuário - função iniciar.

--- sistema.pl
+++ sistema.pl
@@ -99,41 +99,154 @@ pergunta(10, "Voce se sente animado em criar jogos, apps ou sistemas do zero via
 
 % util
 somar_lista([], 0).
-somar_lista([h|t], s) :- somar_lista(t, r), s is h + r.
+somar_lista([H|T], S) :-
+    somar_lista(T, R),
+    S is H + R.
+
+limpar_respostas :- retractall(resposta(_,_)).
 
 % pontuacao por trilha
-calcular_pontuacao_trilha(trilha, p) :-
-    findall(peso,
-        ( perfil(trilha, carac, peso),
-          pergunta(id, _txt, carac),
-          resposta(id, s)
+calcular_pontuacao_trilha(Trilha, Pontuacao) :-
+    findall(Peso,
+        ( perfil(Trilha, Caracteristica, Peso),
+          pergunta(Id, _Txt, Caracteristica),
+          resposta(Id, s)
         ),
-        pesos),
-    somar_lista(pesos, p).
+        Pesos),
+    somar_lista(Pesos, Pontuacao).
 
-calcula_pontuacao(trilha, p, _ctx) :- calcular_pontuacao_trilha(trilha, p).
+calcula_pontuacao(Trilha, Pontuacao, _Ctx) :-
+    calcular_pontuacao_trilha(Trilha, Pontuacao).
 
 % lista (Trilha, Pontuacao) sem ordenar
-todas_pontuacoes(tuplas_tp) :-
-    findall((t, p),
-        ( trilha(t, _), calcular_pontuacao_trilha(t, p) ),
-        tuplas_tp).
+todas_pontuacoes(Tuplas_tp) :-
+    findall((T, P),
+        ( trilha(T, _),
+          calcular_pontuacao_trilha(T, P)
+        ),
+        Tuplas_tp).
 
 % ordenacao decrescente por pontuacao
-comparar_tp_desc(delta, (_T1,P1), (_T2,P2)) :- compare(delta, P2, P1).
+comparar_tp(Delta, (_T1,P1), (_T2,P2)) :-
+    compare(Delta, P2, P1).
 
-ranking_trilha_pontuacao(tuplas_ordenadas) :-
-    todas_pontuacoes(tuplas),
-    predsort(comparar_tp_desc, tuplas, tuplas_ordenadas).
+ranking_trilha_pontuacao(Tuplas_ordenadas) :-
+    todas_pontuacoes(Tuplas),
+    predsort(comparar_tp, Tuplas, Tuplas_ordenadas).
 
-recomenda(ranking_ordenado) :-
-    ranking_trilha_pontuacao(ranking_ordenado).
+% APIs
+recomenda(Ranking_ordenado) :-
+    ranking_trilha_pontuacao(Ranking_ordenado).
 
-recomenda(trilhas_topo, pontuacao_maxima) :-
-    ranking_trilha_pontuacao([(t1,pmax)|resto]),
-    pontuacao_maxima = pmax,
-    findall(t, member((t,pmax), [(t1,pmax)|resto]), trilhas_topo).
+recomenda(Trilhas_topo, Pontuacao_maxima) :-
+    ranking_trilha_pontuacao([(Trilha1,Pmax)|Resto]),
+    Pontuacao_maxima = Pmax,
+    findall(T, member((T,Pmax), [(Trilha1,Pmax)|Resto]), Trilhas_topo).
 
-recomenda_trilha(trilha_escolhida, pontuacao_maxima) :-
-    ranking_trilha_pontuacao([(trilha_escolhida, pontuacao_maxima)|_]).
+recomenda_trilha(Trilha_escolhida, Pontuacao_maxima) :-
+    ranking_trilha_pontuacao([(Trilha_escolhida, Pontuacao_maxima)|_]).
 
+% Interface de usuario
+
+iniciar :-
+    limpar_respostas,
+    writeln("  Sistema de recomendacao de trilhas"),
+    writeln("  Responda apenas com s ou n (sem ponto final)"),
+    obter_ids_perguntas(Ids),
+    perguntar_ids(Ids),
+    ranking_trilha_pontuacao(Ranking),
+    exibir_recomendacao(Ranking).
+
+obter_ids_perguntas(Ids_ordenados) :-
+    findall(Id, pergunta(Id,_,_), Ids),
+    sort(Ids, Ids_ordenados).
+
+perguntar_ids([]).
+perguntar_ids([Id|Resto]) :-
+    pergunta(Id, Texto, _Carac),
+    format("~w~n> ", [Texto]),
+    read_line_to_string(user_input, In0),
+    string_lower(In0, In),
+    ( In = "s" -> Resp = s
+    ; In = "n" -> Resp = n
+    ; writeln("Use s ou n."), !, perguntar_ids([Id|Resto])
+    ),
+    assertz(resposta(Id, Resp)),
+    perguntar_ids(Resto).
+
+% -------- exibicao --------
+
+exibir_recomendacao([]) :-
+    writeln("Nenhuma trilha recomendada.").
+exibir_recomendacao([(Trilha_top, Pmax)|Resto]) :-
+    % coletar todos os empatados no topo
+    findall(T, member((T,Pmax), [(Trilha_top,Pmax)|Resto]), Topos),
+    writeln(""),
+    writeln("Voce se enquadra em:"),
+    listar_topos(Topos, Pmax),
+    writeln(""),
+    writeln("Justificativas (respostas que contaram):"),
+    justificar_topos(Topos),
+    writeln("----------------------------------------------"),
+    writeln("Outras possiveis opcoes:"),
+    filtrar_restantes([(Trilha_top,Pmax)|Resto], Pmax, Restantes),
+    exibir_outros(Restantes).
+
+listar_topos([], _).
+listar_topos([T|Ts], P) :-
+    trilha(T, Desc),
+    format("Trilha: ~w (~w pontos)~n", [T, P]),
+    format("Descricao: ~w~n", [Desc]),
+    listar_topos(Ts, P).
+
+justificar_topos([]).
+justificar_topos([T|Ts]) :-
+    justificar_trilha(T),
+    justificar_topos(Ts).
+
+filtrar_restantes(Lista, Pmax, Restantes_ord) :-
+    include(\=( (_T,Pmax) ), Lista, Sem_topos),
+    % opcional: esconder zeros
+    include(tem_pontos, Sem_topos, Sem_zeros),
+    predsort(comparar_tp, Sem_zeros, Restantes_ord).
+
+tem_pontos((_T,P)) :- P > 0.
+
+exibir_outros([]) :-
+    writeln(" - (nenhuma)").
+exibir_outros(Lista) :-
+    forall(member((T,P), Lista),
+        (   trilha(T, Desc),
+            format(" - ~w: ~w pontos~n", [T, P]),
+            format("   ~w~n", [Desc])
+        )
+    ).
+
+justificar_trilha(Trilha) :-
+    (   findall((Texto,Peso),
+            ( pergunta(Id, Texto, Carac),
+              resposta(Id, s),
+              perfil(Trilha, Carac, Peso),
+              Peso > 0
+            ),
+            Pares),
+        Pares \= []
+    ->  forall(member((Txt,Pz), Pares),
+            format(" - ~w (+~w)~n", [Txt, Pz]))
+    ;   writeln(" - (sem respostas positivas que contem para esta trilha)")
+    ).
+
+% teste local
+carregar_perfil(Arquivo) :-
+    limpar_respostas,
+    consult(Arquivo).
+
+rodar_teste(Arquivo) :-
+    carregar_perfil(Arquivo),
+    ranking_trilha_pontuacao(Ranking),
+    exibir_recomendacao(Ranking).
+
+rodar_todos :-
+    forall(member(F, ['resposta1.pl','resposta2.pl','resposta3.pl']),
+           ( format("== ~w ==~n", [F]),
+             rodar_teste(F), nl )).
